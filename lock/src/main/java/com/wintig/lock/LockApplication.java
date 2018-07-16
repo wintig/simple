c081469de4a70d3b78688af4939b34f6611ce95c
shitian <840872473@qq.com>
2018-07-16 23:25:25 +0800

    初始化项目

---
+++ lock/src/main/java/com/wintig/lock/LockApplication.java
@@ -0,0 +1,12 @@
+package com.wintig.lock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LockApplication.class, args);
+	}
+}
